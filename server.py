b0ef66dd83daab604853b7eb908905771b9667a4
Nikita-Loskutov <nikitaloskutiv@gmail.com>
2025-03-24 14:13:36 +0200

    update

--- server.py
+++ server.py
@@ -1,10 +1,11 @@
-from flask import Flask, request, jsonify, redirect, render_template
+from flask import Flask, request, jsonify, redirect, render_template, flash
 from sqlalchemy.orm import Session
 from models import User
 from database import engine, SessionLocal, Base
 import bcrypt
 
 app = Flask(__name__)
+app.secret_key = 'supersecretkey'
 
 # Create database tables
 Base.metadata.create_all(bind=engine)
@@ -26,6 +27,10 @@ def register():
         db: Session = next(get_db())
         username = request.form["username"]
         password = request.form["password"]
+        
+        if db.query(User).filter(User.username == username).first():
+            flash("Данный ник занят попробуйте другой.", "error")
+            return render_template("register.html")
         
         hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
@@ -49,7 +54,8 @@ def login():
         if user and bcrypt.checkpw(password.encode('utf-8'), user.hashed_password.encode('utf-8')):
             return render_template("suc.html")
         else:
-            return render_template("no.html")
+            flash("Неправильный логин или пороль!", "error")
+            return render_template("login.html")
     return render_template("login.html")
 
 if __name__ == "__main__":
